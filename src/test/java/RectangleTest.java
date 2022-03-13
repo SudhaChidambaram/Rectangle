ee5dd1cd44b41c29aa7ecffd057fa95cbf43fba9
sudha <sudha@sudhas-MacBook-Pro.local>
2022-03-13 17:48:16 +0530

    [Navigator] Sudha Testcase3 to calculate perimeter of Rectangle with int values added

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test;
 
 public class RectangleTest {
     @Test
-    void areaIfHeight3AndWidth4Is12() {
-        Rectangle rectangle =  Rectangle.createRectangle(3, 4);
+    void areaOfRectangleIfHeight3AndWidth4Is12() {
+        Rectangle rectangle = Rectangle.createRectangle(3, 4);
 
         double area = rectangle.area();
 
@@ -13,13 +13,24 @@ public class RectangleTest {
     }
 
     @Test
-    void areaIfHeight3Point5AndWidth2Pont5Is9Point45() {
-        Rectangle rectangle = Rectangle.createRectangle(3.5,2.5);
+    void areaOfRectangleIfHeight3Point5AndWidth2Pont5Is9Point45() {
+        Rectangle rectangle = Rectangle.createRectangle(3.5, 2.5);
 
         double area = rectangle.area();
 
         Assertions.assertEquals(8.75, area);
     }
+
+    @Test
+    void perimeterOfRectangleWithHeight2AndWidth4Is12() {
+        Rectangle rectangle = Rectangle.createRectangle(2, 4);
+
+        double perimeter = rectangle.perimeter();
+
+        Assertions.assertEquals(12, perimeter);
+    }
+
+
 }
 
 

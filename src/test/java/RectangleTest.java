50b3a9026688eb6b24756f65340bd1a17a9c2fca
sudha <sudha@sudhas-MacBook-Pro.local>
2022-03-13 18:11:46 +0530

    [Navigator] Sudha Testcase to test perimeter of square added

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -8,27 +8,36 @@ public class RectangleTest {
     void areaOfRectangleIfHeight3Point5AndWidth2Pont5Is9Point45() {
         Rectangle rectangle = Rectangle.createRectangle(3.5, 2.5);
 
-        double area = rectangle.area();
+        double areaRectangle = rectangle.area();
 
-        Assertions.assertEquals(8.75, area);
+        Assertions.assertEquals(8.75, areaRectangle);
     }
 
     @Test
     void perimeterOfRectangleWithHeight2Point5AndWidth4Point1Is13Point2() {
         Rectangle rectangle = Rectangle.createRectangle(2.5, 4.1);
 
-        double perimeter = rectangle.perimeter();
+        double perimeterRectangle = rectangle.perimeter();
 
-        Assertions.assertEquals(13.2, perimeter);
+        Assertions.assertEquals(13.2, perimeterRectangle);
     }
 
     @Test
-    void areaOfSquareIfHeight3Point5AndWidth3Pont5Is102Point25() {
+    void areaOfSquareWithSide3Point5IsPoint25() {
         Rectangle square = Rectangle.createSquare(3.5);
 
-        double area = square.area();
+        double areaSquare = square.area();
 
-        Assertions.assertEquals(12.25, area);
+        Assertions.assertEquals(12.25, areaSquare);
+    }
+
+    @Test
+    void perimeterOfSquareWithSide7Point1Is28Point4() {
+        Rectangle square = Rectangle.createSquare(7.1);
+
+        double perimeterSquare = square.perimeter();
+
+        Assertions.assertEquals(28.4, perimeterSquare);
     }
 }
 
